f6bb59b9bbe2f1593e199489674d94c751ab27aa
Aleksan-dres <galeks382@gmail.com>
2025-05-26 00:30:13 +0300

    Update NASA_images.py
    Доработки кода

--- NASA_images.py
+++ NASA_images.py
@@ -5,17 +5,19 @@ import os
 import requests
 from dotenv import load_dotenv
 from urllib.parse import urlparse
+from file import made_file
+
+ 
+def download_file(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    with open(filename, 'wb') as foto_space:
+        foto_space.write(response.content)
 
 
-if not os.path.exists("foto_space"):
-    os.mkdir("foto_space")
-foto_space = () 
-
-
-url_nasa = "https://api.nasa.gov/planetary/apod"
-
-
-def image_nasa_every_day(url_nasa,nasa_token,initial_date,final_date): 
+def download_image_nasa_every_day(nasa_token,initial_date,final_date): 
+    url_nasa = "https://api.nasa.gov/planetary/apod" 
     payload = {
         "start_date": initial_date,
         "end_date": final_date,
@@ -26,7 +28,7 @@ def image_nasa_every_day(url_nasa,nasa_token,initial_date,final_date):
     foto_every_day = response.json()  
     
     
-    for i, image in enumerate(foto_every_day): 
+    for value, image in enumerate(foto_every_day): 
         if not foto_every_day or 'media_type' not in image or image['media_type'] != 'image': 
             continue 
         foto_url = image["url"] 
@@ -37,26 +39,20 @@ def image_nasa_every_day(url_nasa,nasa_token,initial_date,final_date):
         if format_foto != ".jpg": 
             continue 
 
-        filename_1 = f"foto_space/nasa{i}.jpg"
+        filename_1 = f"foto_space/nasa{value}.jpg"
 
-        response = requests.get(foto_url)
-        response.raise_for_status()
-
-        with open(filename_1, 'wb') as foto_space:
-            foto_space.write(response.content) 
+        download_file(foto_url, filename_1)
 
 
 
 def main(): 
-    
-    
     load_dotenv()
-    nasa_token = os.environ["NASA_API"]  
+    nasa_token = os.environ["NASA_TOKEN"]  
     date_now = datetime.datetime.now()
-    monat = date_now - datetime.timedelta(days=30)
+    days_30 = date_now - datetime.timedelta(days=30)
 
     parser = argparse.ArgumentParser(description="Загружает лучшее космические фото дня")
-    parser.add_argument('-start_date', help='Введите дату начала отсчета(год(4 числа),месяц(2числа),день(2 числа))', default=monat.strftime("%Y-%m-%d"))
+    parser.add_argument('-start_date', help='Введите дату начала отсчета(год(4 числа),месяц(2числа),день(2 числа))', default=days_30.strftime("%Y-%m-%d"))
     parser.add_argument('-end_date', help='Введите дату окончания отсчета(год(4 числа),месяц(2числа),день(2 числа))', default=datetime.datetime.now().strftime("%Y-%m-%d")) 
      
     args = parser.parse_args()
@@ -65,7 +61,7 @@ def main():
     final_date = args.end_date
     
 
-    image_nasa_every_day(url_nasa,nasa_token,initial_date,final_date)  
+    download_image_nasa_every_day(nasa_token, initial_date, final_date)  
         
 if __name__ == '__main__':
-    main()  
+    main() 
