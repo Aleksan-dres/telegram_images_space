db11a2e10d096c41791f0b9f7fd2eeb9e96aa4c0
Aleksan-dres <galeks382@gmail.com>
2025-05-27 22:33:59 +0300

    Update one_foto_telegram.py
    Изменил названия переменной telegram_token. Добавил новую переменную telegram_chat_id

--- one_foto_telegram.py
+++ one_foto_telegram.py
@@ -5,14 +5,14 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    telegram_token = os.environ['telegram_TOKEN']
-    
-    bot = telegram.Bot(token)
+    telegram_token = os.environ['TELEGRAM_TOKEN']
+    telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
+    bot = telegram.Bot(telegram_token)
         
     file_path = 'foto_space/spacex2.jpg'
 
     with open(file_path, 'rb') as file:
-        bot.send_document(chat_id='@spaccce_images', document=file)
+        bot.send_document(chat_id=telegram_chat_id, document=file)
 
 
 if __name__ == '__main__':
