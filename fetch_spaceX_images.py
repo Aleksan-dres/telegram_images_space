2b87a1529d08697cdb9257123a9f6e388a628363
Aleksan-dres <galeks382@gmail.com>
2025-05-27 22:25:04 +0300

    Update fetch_spaceX_images.py
    Убрал import file.py. добавил import download_and_save. Изменил название переменной в цикле на number. Изменил название переменной в функции fetch_spacex_last_launch на spacex_image. Подправил регистр букв названии переменных id_spacex

--- fetch_spaceX_images.py
+++ fetch_spaceX_images.py
@@ -2,34 +2,28 @@ import requests
 import json
 import os 
 import argparse 
-from file import made_file
+from download_and_save_fetch_spacex_image import download_file
 
-def download_file(url, filename):
-    response = requests.get(url)
-    response.raise_for_status()
-    
-    with open(filename, 'wb') as foto_space:
-        foto_space.write(response.content)
 
 def fetch_spacex_last_launch(ID_space_x):
     response = requests.get(f"https://api.spacexdata.com/v5/launches/{ID_space_x}")
     response.raise_for_status()
-    foto_x = response.json()["links"]['flickr']['original'] 
-    if not foto_x: 
+    photos_spacex = response.json()["links"]['flickr']['original'] 
+    if not photos_spacex: 
         print("Фотографии отсутствуют для данного запуска")
         return  
 
-    for image, value in enumerate(foto_x):
-        filename_1 = f"foto_space/spacex{image}.jpg"
-        download_file(value, filename)
+    for image, number in enumerate(photos_spacex):
+        spacex_image = f"foto_space/spacex{image}.jpg"
+        download_file(number, spacex_image)
 
 def main(): 
     parser = argparse.ArgumentParser(description="Загружает фото компании SpaceX")
-    parser.add_argument('-ID_spaceX', help="Укажите ID для загрузки фотографий с запуска, если ID нет используйте строку (latest)", default="latest") 
+    parser.add_argument('-id_spacex', help="Укажите id для загрузки фотографий с запуска, если ID нет используйте строку (latest)", default="latest") 
     args = parser.parse_args()
 
-    ID_space_x = args.ID_spaceX 
-    fetch_spacex_last_launch(ID_space_x) 
+    launch_spacex = args.id_spacex 
+    fetch_spacex_last_launch(id_space_x) 
 
 if __name__ == '__main__':
     main()  
