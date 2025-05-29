5dd1368a76c3aa69fa36bdcdde0b7a0a94cb4a3d
Aleksan-dres <galeks382@gmail.com>
2025-05-29 23:12:52 +0300

    Update one_foto_telegram.py
    Добавил переменную имя фотографии photo_space. Указал путь к файлу file_path

--- one_foto_telegram.py
+++ one_foto_telegram.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    telegram_token = os.environ['TELEGRAM_TOKEN']
+    telegram_token = os.environ['telegram_TOKEN']
     telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
-    bot = telegram.Bot(telegram_token)
-        
-    file_path = 'foto_space/spacex2.jpg'
+    bot = telegram.Bot(token)
+    photo_space = "space2.jpg"
+    file_path = f'foto_space/{photo_space}'
 
     with open(file_path, 'rb') as file:
         bot.send_document(chat_id=telegram_chat_id, document=file)
