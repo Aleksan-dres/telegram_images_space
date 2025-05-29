7fa5c498d28d5e56e710a99c33b6d275b584864d
Aleksan-dres <galeks382@gmail.com>
2025-05-29 23:01:10 +0300

    Update fetch_spaceX_images.py
    Переименовал файл в import download_and_save_file. Поправил название id_spacex. Указал в функции новое название переменной launch_spacex. Поправил название переменной spacex_photos

--- fetch_spaceX_images.py
+++ fetch_spaceX_images.py
@@ -2,20 +2,22 @@ import requests
 import json
 import os 
 import argparse 
-from download_and_save_fetch_spacex_image import download_file
+from download_and_save_fetch_spacex_image import download_and_save_file
 
 
-def fetch_spacex_last_launch(ID_space_x):
-    response = requests.get(f"https://api.spacexdata.com/v5/launches/{ID_space_x}")
+def fetch_spacex_last_launch(launch_spacex):
+    response = requests.get(f"https://api.spacexdata.com/v5/launches/{launch_spacex}")
     response.raise_for_status()
-    photos_spacex = response.json()["links"]['flickr']['original'] 
-    if not photos_spacex: 
+    spacex_photos = response.json()["links"]['flickr']['original'] 
+    if not spacex_photos: 
         print("Фотографии отсутствуют для данного запуска")
         return  
 
-    for image, number in enumerate(photos_spacex):
-        spacex_image = f"foto_space/spacex{image}.jpg"
-        download_file(number, spacex_image)
+    for image, number in enumerate(spacex_photos): 
+        spacex_image = f"spacex{image}.jpg" 
+        path_to_file_with_photos = f"foto_space/{spacex_image}"
+        
+        download_and_save_file(number, path_to_file_with_photos)
 
 def main(): 
     parser = argparse.ArgumentParser(description="Загружает фото компании SpaceX")
@@ -23,7 +25,7 @@ def main():
     args = parser.parse_args()
 
     launch_spacex = args.id_spacex 
-    fetch_spacex_last_launch(id_space_x) 
+    fetch_spacex_last_launch(launch_spacex) 
 
 if __name__ == '__main__':
-    main()  
+    main() 
