a0ea0ea7d7dd3f2f625efd6485571a84ffa0ae4b
Aleksan-dres <galeks382@gmail.com>
2025-05-30 23:26:01 +0300

    Update images_telegram.py
    Вынес константы, переименовал функцию generate_paths. Переименовал переменную file_paths

--- images_telegram.py
+++ images_telegram.py
@@ -8,6 +8,9 @@ from dotenv import load_dotenv
 from PIL import Image
 
 
+BYTES_IN_MEGABYTES = 1048576 
+PHOTO_SIZE_IN_MEGABYTES = 20
+
 def optimize_photo(file_path): 
     filename, file_extension = os.path.splitext(file_path) 
     optimized_image = f"{filename}_optimized{file_extension}"
@@ -17,16 +20,15 @@ def optimize_photo(file_path):
     
     return optimized_image 
 
-def creating_list_of_file(folder_path): 
-    file_names = []
-    for file_name in os.listdir(folder_path):
-        full_path = os.path.join(folder_path, file_name)
+def generate_paths(folder_path): 
+    file_paths = []
+    for file_path in os.listdir(folder_path):
+        full_path = os.path.join(folder_path, file_path)
         if os.path.isfile(full_path):
-            file_names.append(full_path) 
-    return file_names
+            file_paths.append(full_path) 
+    return file_paths
 
 def main():
-
     load_dotenv()
     telegram_token = os.environ['TELEGRAM_TOKEN'] 
     telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
@@ -40,17 +42,15 @@ def main():
     
 
     while True:
-        megabytes = 1048576 
-        photo_size = 20
         folder_path = 'foto_space'
-        file_names = creating_list_of_file(folder_path)
+        file_paths = generate_paths(folder_path)
 
-        random.shuffle(file_names)
+        random.shuffle(file_paths)
 
-        file_path = file_names[0]
+        file_path = file_paths[0]
         file_size = os.path.getsize(file_path)
-        file_size_megabytes = file_size / megabytes
-        if file_size_megabytes >= photo_size:
+        file_size_megabytes = file_size / BYTES_IN_MEGABYTES
+        if file_size_megabytes >= PHOTO_SIZE_IN_MEGABYTES:
             upload_path = optimize_photo(file_path) 
             
         else:
